06c283693576954042be128455304b9d7a842293
TechSammy <mrsam03102002@gmail.com>
2024-03-13 08:00:28 +0530

    Namaste React Episode 1-done! :)

---
+++ README.md
@@ -0,0 +1,1 @@
+# learning react from scratch
---
+++ app.js
@@ -0,0 +1,33 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading", abc: "adb" },
+  "Hello from react"
+);
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement(
+      "h1",
+      { id: "heading", abc: "adb" },
+      "H1 tag from child 1"
+    ),
+    React.createElement(
+      "h2",
+      { id: "heading", abc: "adb" },
+      "H2 tag from child 1"
+    ),
+  ]), React.createElement("div", { id: "child2" }, [
+    React.createElement(
+      "h1",
+      { id: "heading", abc: "adb" },
+      "H1 tag from child 2"
+    ),
+    React.createElement(
+      "h2",
+      { id: "heading", abc: "adb" },
+      "H2 tag from child 2"
+    ),
+  ])
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
