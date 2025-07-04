ef9399e0a642ff80a19dba343d887cfc0c9fdf37
CodeWithSammy <mrsam03102002@gmail.com>
2025-07-04 13:55:44 +0530

    inception ep1

---
+++ App.js
@@ -0,0 +1,15 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "grandchild" }, "H1 tag 1"),
+    React.createElement("h1", { id: "grandchild" }, "H1 tag 2"),
+  ]),React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "grandchild" }, "H1 tag 1.1"),
+    React.createElement("h1", { id: "grandchild" }, "H1 tag 2.2"),
+  ])]
+);
+
+const heading = React.createElement("h1", {}, "Hello, World!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
