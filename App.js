831ee8ed771631e51ecb2574fc24ec4f26faec6e
CodeWithSammy <mrsam03102002@gmail.com>
2025-07-04 15:34:28 +0530

    ignite your app ep2

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
-  "div",
+  "div",    
   { id: "parent" },
   [React.createElement("div", { id: "child" }, [
     React.createElement("h1", { id: "grandchild" }, "H1 tag 1"),
