56011b63f5a1e8e1e09720b81793592b00b0a61d
CodeWithSammy <mrsam03102002@gmail.com>
2025-07-06 01:51:24 +0530

    talk is cheap show me the code ep4
    loading data dynamically with props

--- App.js
+++ App.js
@@ -1,22 +1,80 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => {
-  return <h1 className="head" tabIndex="5">Namaste React hahahaha</h1>;
-};
-const number =100+20;
-const HeadingComponent = () => {
+const resList = [
+  {
+    resName: "Meghana Foods",
+  },
+  {
+    resName: "KFC",
+  },
+  {
+    resName: "McDonald's",
+  },
+  {
+    resName: "Dominos",
+  },
+];
+const Header = () => {
   return (
-    <div className="heading-container">
-      <Title />
-      {Title()}
-      <Title></Title>
-      {number}
-      <h1 className="heading">Namaste React via Functional component </h1>
+    <div className="header">
+      <img
+        className="logo"
+        src="https://dynamic.brandcrowd.com/asset/logo/5c6cee11-6c92-402c-bda4-e2c11ec15db3/logo-search-grid-1x?logoTemplateVersion=3&v=638584435499400000"
+        alt="Namaste React Logo"
+      />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const ResCard = (props) => {
+    
+    const { resData } = props;
+    console.log(props);
+
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src="https://dynamic.brandcrowd.com/asset/logo/5c6cee11-6c92-402c-bda4-e2c11ec15db3/logo-search-grid-1x?logoTemplateVersion=3&v=638584435499400000"
+        alt="Restaurant Logo"
+      />
+      <h3>{resData.resName}</h3>
+      <a>Cuisine</a>
+      <a>4.3 Rating</a>
+      <a>Delivery Time</a>
+      <a>Cost for Two</a>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {resList.map((restaurant, index) => (
+          <ResCard key={index} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
